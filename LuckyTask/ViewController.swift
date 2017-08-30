b6a2993c9437cbecb0e7e07e8c76944ede69555d
Abdalla Odat <macbook@Macbooks-MacBook-Pro.local>
2017-08-31 00:57:20 +0300

    Add reference of home page outlets

--- LuckyTask/ViewController.swift
+++ LuckyTask/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var showSelected = false
+    
+    @IBOutlet weak var selectedCountry: UILabel!
+    @IBOutlet weak var selectedAirport: UILabel!
+    @IBOutlet weak var countryImage: UIImageView!
+    @IBOutlet weak var airportImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let userDefault = UserDefaults.standard
+        if showSelected == true {
+            selectedCountry.text = userDefault.string(forKey: "countryName")
+            selectedAirport.text = userDefault.string(forKey: "airportName")
+            countryImage.af_setImage(withURL:URL(string: userDefault.string(forKey: "countryImage")!)!)
+            airportImage.af_setImage(withURL:URL(string: userDefault.string(forKey: "airportImage")!)!)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
